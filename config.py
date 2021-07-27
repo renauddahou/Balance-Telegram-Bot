a27100e0cb491199808ece58ffbba77b523e2003
renaudatascience <85571576+renauddahou@users.noreply.github.com>
2021-07-27 12:39:12 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import logging
 
 
 logging_level = logging.INFO
-bot_token = os.environ.get('BOT_TOKEN')
+bot_token = os.environ.get('1897550776:AAEedZUqU66DKQYWXUq0m60DqepSyTeYFQY')
 aviable_chats = []
 counter_max = 25
-messages_url = ''
-everyday_money = ''
+messages_url = 'wastechains'
+everyday_money = '1dollars'
